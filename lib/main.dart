511c84b8b979fa66358d6c8a2db8ea4d2d3a62c9
Laurent Quenel <laurent@macbook-pro-de-laurent-2.home>
2022-04-12 11:07:49 +0200

    flutter fire

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase/firebase.dart' as WebFirebase;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
