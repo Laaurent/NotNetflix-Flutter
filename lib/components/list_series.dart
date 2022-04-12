b6017dfebc1d511bee576a6fb08428bd56b3c90a
Florian PIAULT <florian.piault@mail-ecv.fr>
2022-04-12 17:00:46 +0200

    list series

--- lib/components/list_series.dart
+++ lib/components/list_series.dart
@@ -15,7 +15,7 @@ class _ListSeriesState extends State<ListSeries> {
   List<dynamic> series = [];
   int _currentPage = 0;
 
-  void fetchData(page) async {
+  Future<List<dynamic>> fetchData(page) async {
     final Uri uri =
         Uri.parse('https://api.tvmaze.com/shows?page=' + page.toString());
     final response = await http.get(uri);
@@ -24,6 +24,7 @@ class _ListSeriesState extends State<ListSeries> {
     setState(() {
       series = List.from(series)..addAll(_series);
     });
+    return Future(() => series);
   }
 
   void fetchMore() {
@@ -39,6 +40,66 @@ class _ListSeriesState extends State<ListSeries> {
     fetchData(_currentPage);
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return FutureBuilder(
+  //       future: fetchData(
+  //           _currentPage), // the function to get your data from firebase or firestore
+  //       builder: (BuildContext context, AsyncSnapshot snap) {
+  //         if (snap.data == null) {
+  //           return const Text('LOADING...');
+  //         } else {
+  //           return Column(
+  //             children: [
+  //               Padding(
+  //                 padding: const EdgeInsets.only(left: 10),
+  //                 child: Wrap(
+  //                   children: [
+  //                     Text(
+  //                       'Tendences actuelles' + snap.data.length.toString(),
+  //                       style: const TextStyle(
+  //                           fontSize: 20, fontWeight: FontWeight.bold),
+  //                     ),
+  //                     SizedBox(
+  //                       height: 200,
+  //                       child: ListView.builder(
+  //                         scrollDirection: Axis.horizontal,
+  //                         itemCount: series.length,
+  //                         itemBuilder: (context, index) {
+  //                           final oneSeries = snap.data[index];
+  //                           final image = oneSeries['image']['medium'] != null
+  //                               ? oneSeries['image']['medium']
+  //                               : oneSeries['image']['original'] != null
+  //                                   ? oneSeries['image']['original']
+  //                                   : 'https://www.placecage.com/640/360';
+  //                           return Container(
+  //                             margin: const EdgeInsets.only(right: 10),
+  //                             width: 125,
+  //                             height: 200,
+  //                             decoration: BoxDecoration(
+  //                               borderRadius: const BorderRadius.only(
+  //                                   topLeft: Radius.circular(10),
+  //                                   topRight: Radius.circular(10),
+  //                                   bottomLeft: Radius.circular(10),
+  //                                   bottomRight: Radius.circular(10)),
+  //                               image: DecorationImage(
+  //                                 image: NetworkImage(image),
+  //                                 fit: BoxFit.cover,
+  //                               ),
+  //                             ),
+  //                           );
+  //                         },
+  //                       ),
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ),
+  //             ],
+  //           );
+  //         }
+  //       });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -48,9 +109,10 @@ class _ListSeriesState extends State<ListSeries> {
             padding: const EdgeInsets.only(left: 10),
             child: Wrap(
               children: [
-                const Text(
-                  'Tendences actuelles',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                Text(
+                  'Tendences actuelles' + series.length.toString(),
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(
                   height: 200,
