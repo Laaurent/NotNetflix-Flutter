9a45da085e7e7576d9f9bc966beac50e18d580bc
Florian PIAULT <florian.piault@mail-ecv.fr>
2022-04-12 15:18:05 +0200

    home

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_if_null_operators
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,11 +13,38 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   int _currentTab = 1;
+  int _currentPage = 0;
+  List<dynamic> series = [];
+  // late Future<Series> series;
   final List<Map<String, dynamic>> _tabs = const [
     {'name': 'Profil', 'icon': Icon(Icons.person)},
     {'name': 'Home', 'icon': Icon(Icons.home)},
     {'name': 'Search', 'icon': Icon(Icons.search)},
   ];
+
+  void fetchData(page) async {
+    final Uri uri =
+        Uri.parse('https://api.tvmaze.com/shows?page=' + page.toString());
+    final response = await http.get(uri);
+    final _series = jsonDecode(response.body) as List;
+
+    setState(() {
+      series = List.from(series)..addAll(_series);
+    });
+  }
+
+  void fetchMore() {
+    setState(() {
+      _currentPage += 1;
+    });
+    fetchData(_currentPage + 1);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(_currentPage);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +52,56 @@ class HomeState extends State<Home> {
       appBar: AppBar(
         title: Text(_tabs[_currentTab]['name']),
       ),
-      body: null,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Wrap(
+              children: [
+                const Text(
+                  'Tendences actuelles',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 200,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: series.length,
+                    itemBuilder: (context, index) {
+                      final oneSeries = series[index];
+                      final image = oneSeries['image']['medium'] != null
+                          ? oneSeries['image']['medium']
+                          : oneSeries['image']['original'] != null
+                              ? oneSeries['image']['original']
+                              : 'https://www.placecage.com/640/360';
+                      return Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        width: 125,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10)),
+                          image: DecorationImage(
+                            image: NetworkImage(image),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => fetchMore(),
+        child: const Icon(Icons.add),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentTab,
         onTap: (index) {
